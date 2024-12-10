a5302917b15754852f8a594820ded6a72aad8093
Jeremy Weiss <jlweiss@email.arizona.edu>
2024-12-10 11:19:20 -0700

    initial commit

---
+++ app/app.R
@@ -0,0 +1,25 @@
+# View 15-minute data in tables and time series
+
+# Libraries
+library(azmetr)
+
+# Functions 
+#source("./R/fxn_ABC.R", local = TRUE)
+
+# Scripts
+#source("./R/scr##_DEF.R", local = TRUE)
+
+
+# UI --------------------
+
+ui <- 
+
+
+# Server --------------------
+
+server <- function(input, output, session) {}
+
+
+# Run --------------------
+
+shinyApp(ui, server)
