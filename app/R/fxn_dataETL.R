750206fcc21556f4b91927166ea41400dc88b8c5
Jeremy Weiss <jlweiss@email.arizona.edu>
2025-01-13 15:41:12 -0700

    data value formatting for station-level-summaries download

--- app/R/fxn_dataETL.R
+++ app/R/fxn_dataETL.R
@@ -90,7 +90,55 @@ fxn_dataETL <- function() {
       wind_2min_vector_dir_max_daily,
       wind_2min_spd_max_mph_hourly,
       wind_2min_vector_dir_max_hourly
-    )  |>
+    )   |>
+    
+    dplyr::mutate(
+      dplyr::across(
+        c(
+          "relative_humidity",
+          "wind_vector_dir",
+          "wind_2min_vector_dir",
+          "wind_2min_vector_dir_max_daily",
+          "wind_2min_vector_dir_max_hourly"
+        ),
+        \(x) round(x, digits = 0)
+      )
+    ) |>
+    
+    dplyr::mutate(
+      dplyr::across(
+        c(
+          "temp_airF",
+          "dwptF",
+          "temp_air_maxF",
+          "temp_air_minF",
+          "temp_panelF",
+          "temp_wetbulbF",
+          "temp_soil_10cmF",
+          "temp_soil_50cmF",
+          "wind_spd_mph",
+          "wind_spd_max_mph",
+          "wind_2min_spd_mean_mph",
+          "wind_2min_spd_max_mph_daily",
+          "wind_2min_spd_max_mph_hourly"
+        ),
+        \(x) round(x, digits = 1)
+      )
+    ) |>
+    
+    dplyr::mutate(
+      dplyr::across(
+        c(
+          "meta_bat_volt",
+          "precip_total_in",
+          "sol_rad_Wm2",
+          "vp_saturation",
+          "vp_actual",
+          "vp_deficit"
+        ),
+        \(x) round(x, digits = 2)
+      )
+    ) |>
     
     dplyr::arrange(meta_station_name)
   
